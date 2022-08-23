459c107a1477ec1731c8cb3baa780a57fe559861
Kacy Clark <kacy@usa.com>
2022-08-22 23:14:29 -0400

    genesis

---
+++ Blog/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    # success_url = reverse("login")
+    
